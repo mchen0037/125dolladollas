7b3e153786c8be7f6c3dbc4ed24c4b998f7af7bf
Jesus Sanchez <jsanchez254@ucmerced.edu>
2018-11-04 10:27:51 -0800

    image added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
+import mapFrame from './components/mapFrame';
+
+//IMPORT SEMANTIC-UI
+import {Container,Header, Divider, Grid} from "semantic-ui-react";
+
+//IMPORT IMAGES
+import campus from "./assets/img/campus.jpg";
+
+//IMPORT COMPONENTS
+import MapFrame from "./components/mapFrame";
 
 class App extends Component {
+  state = {
+    campus : campus
+  }
   render() {
+
     return (
       <React.Fragment>
-        HELLO
+        <Container>
+          <MapFrame campus = {this.state.campus}></MapFrame>
+        </Container>
       </React.Fragment>
     );
   }
--- src/components/mapFrame.jsx
+++ src/components/mapFrame.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 
+
 class  mapFrame extends Component {
     state = {  }
     render() { 
         return (  
             <React.Fragment>
-
+                <img src = {this.props.campus}/>
             </React.Fragment>
         );
     }
