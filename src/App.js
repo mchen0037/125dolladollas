88c145bf702171490f9c73ae8aa39d8ca0a90528
Jesus Sanchez <jsanchez254@ucmerced.edu>
2018-11-04 11:27:12 -0800

    menu added

--- src/App.js
+++ src/App.js
@@ -9,6 +9,10 @@ import campus from "./assets/img/campus.jpg";
 
 //IMPORT COMPONENTS
 import MapFrame from "./components/mapFrame";
+import MenuEnergy from "./components/menuEnergy";
+
+//IMPORT CSS
+import "./assets/css/mapFrame.css";
 
 class App extends Component {
   state = {
@@ -18,9 +22,15 @@ class App extends Component {
 
     return (
       <React.Fragment>
-        <Container>
-          <MapFrame campus = {this.state.campus}></MapFrame>
-        </Container>
+        <br/>
+        <div className = "columns">
+            <div className = "column is-6 is-offset-1">
+              <MapFrame campus = {this.state.campus}></MapFrame>
+            </div>
+            <div className = "columns is-3 is-offset-1">
+                <MenuEnergy></MenuEnergy>
+            </div>
+        </div>
       </React.Fragment>
     );
   }
--- src/components/mapFrame.jsx
+++ src/components/mapFrame.jsx
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 
+import {Icon} from "semantic-ui-react";
+
+//<Icon name = "life ring outline"/><span id = "less1">More Info</span>
 
 class  mapFrame extends Component {
     state = {  }
     render() { 
         return (  
             <React.Fragment>
-                <img src = {this.props.campus}/>
+                <img className = "frame1" src = {this.props.campus}/>
             </React.Fragment>
         );
     }
