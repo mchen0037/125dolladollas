3e661995653e003cbd2dfe4bb383bc922f4d1fd3
Jesus Sanchez <jsanchez254@ucmerced.edu>
2018-11-04 14:28:45 -0800

    data bar

--- src/components/timeLine.jsx
+++ src/components/timeLine.jsx
@@ -5,7 +5,7 @@ class timeLine extends Component {
     render() { 
         return ( 
             <React.Fragment>
-                <div className = "line" onMouseDown = {this.props.circleOver}>
+                <div onMouseDown = {this.props.circleOver}  className = "line">
                     <div id = "circle1" className = "circle"></div>
                 </div>
             </React.Fragment>
